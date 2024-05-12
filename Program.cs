ec58b87db4b4a759ba49faff9b75af5364b35d17
LENOVO <LENOVO@DESKTOP-56GTG9S>
2024-05-12 16:27:42 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Namespace is a collection of classes
+using System;
+class Program
+{
+    static void Main1(string[] args)
+    {
+        Console.WriteLine("Heloo");
+    }
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Main1([]);
+    }
+
+
+}
